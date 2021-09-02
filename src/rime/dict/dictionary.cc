84c8799b387489db1c690394fb9670417eabd4f8
Alex Man <alex@alexman.space>
2021-09-02 16:52:30 -0700

    Revert "Sort the first N entries returning from dictionary to improve correction candidate ordering."
    This reverts commit 21e502a6db84c2f15eaafd4ff54a08bf31419c7c.

--- src/rime/dict/dictionary.cc
+++ src/rime/dict/dictionary.cc
@@ -87,10 +87,9 @@ void DictEntryIterator::AddChunk(dictionary::Chunk&& chunk) {
 void DictEntryIterator::Sort() {
   auto& chunks = query_result_->chunks;
   // partial-sort remaining chunks, move best match to chunk_index_
-  size_t sort_first_n = std::min((size_t)30, (size_t)std::distance(chunks.begin() + chunk_index_, chunks.end()));
   std::partial_sort(
       chunks.begin() + chunk_index_,
-      chunks.begin() + chunk_index_ + sort_first_n,
+      chunks.begin() + chunk_index_ + 1,
       chunks.end(),
       dictionary::compare_chunk_by_head_element);
 }
